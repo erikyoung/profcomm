f9db7ac09a49dcacb8986a44b18d5b3b4d9d6a76
ErikYoung <erik.young@rmit.edu.vn>
2017-03-16 17:12:42 +0700

    add design to course page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+before_action :authenticate_user!
+before_action :require_authorized_for_current_course
+
   def show
   end
 
@@ -7,5 +10,12 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_authorized_for_current_course
+   if  !current_user.enrolled_in?(current_lesson.section.course)
+      flash[:alert] ="Invalid login"
+      redirect_to course_path(current_lesson.section.course)
+    end
   end
 end
